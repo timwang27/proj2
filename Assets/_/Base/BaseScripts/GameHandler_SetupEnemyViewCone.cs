8b5223cffe8954a3dbd286e52951132aef2009ee
Tim <timothy.wang@berkeley.edu>
2023-02-21 19:52:46 -0800

    add field of view scene

---
+++ Assets/_/Base/BaseScripts/GameHandler_SetupEnemyViewCone.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey;
+using CodeMonkey.Utils;
+using CodeMonkey.MonoBehaviours;
+
+public class GameHandler_SetupEnemyViewCone : MonoBehaviour {
+
+    [SerializeField] private CameraFollow cameraFollow;
+    [SerializeField] private Player player;
+
+    private void Start() {
+        cameraFollow.Setup(GetCameraPosition, () => 80f, true, true);
+
+        //FunctionPeriodic.Create(SpawnEnemy, 2f);
+        //SpawnEnemy();
+        //EnemyHandler.Create(new Vector3(20, 0));
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.R)) {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+        }
+    }
+
+    private Vector3 GetCameraPosition() {
+        return player.GetPosition();
+    }
+
+    private void SpawnEnemy() {
+        Vector3 spawnPosition = player.GetPosition() + UtilsClass.GetRandomDir() * 60f;// 100f;
+        EnemyHandler enemyHandler = EnemyHandler.Create(spawnPosition);
+        enemyHandler.SetGetTarget(() => player);
+    }
+
+}
---
+++ Assets/_/Base/BaseScripts/Player.cs
@@ -0,0 +1,128 @@
+﻿/* 
+    ------------------- Code Monkey -------------------
+
+    Thank you for downloading this package
+    I hope you find it useful in your projects
+    If you have any questions let me know
+    Cheers!
+
+               unitycodemonkey.com
+    --------------------------------------------------
+ */
+
+using System;
+using UnityEngine;
+using V_AnimationSystem;
+using CodeMonkey.Utils;
+using CodeMonkey;
+
+/*
+ * Player movement with WASD
+ * */
+public class Player : MonoBehaviour, EnemyHandler.IEnemyTargetable {
+    
+    public static Player instance;
+
+    private const float SPEED = 50f;
+    
+    private Player_Base playerBase;
+    private Rigidbody2D playerRigidbody2D;
+    private Vector3 moveDir;
+    private State state;
+    private int health;
+
+    private enum State {
+        Normal,
+    }
+
+    private void Awake() {
+        instance = this;
+        playerBase = gameObject.GetComponent<Player_Base>();
+        playerRigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+        SetStateNormal();
+        health = 4;
+    }
+
+    private void Update() {
+        switch (state) {
+        case State.Normal:
+            HandleMovement();
+            break;
+        }
+    }
+    
+    private void SetStateNormal() {
+        state = State.Normal;
+    }
+
+    private void HandleMovement() {
+        float moveX = 0f;
+        float moveY = 0f;
+        
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
+            moveY = +1f;
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) {
+            moveY = -1f;
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
+            moveX = -1f;
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
+            moveX = +1f;
+        }
+
+        moveDir = new Vector3(moveX, moveY).normalized;
+    }
+
+    private void FixedUpdate() {
+        bool isIdle = moveDir.x == 0 && moveDir.y == 0;
+        if (isIdle) {
+            playerBase.PlayIdleAnim();
+        } else {
+            playerBase.PlayMoveAnim(moveDir);
+            //transform.position += moveDir * SPEED * Time.deltaTime;
+            playerRigidbody2D.MovePosition(transform.position + moveDir * SPEED * Time.fixedDeltaTime);
+        }
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider) {
+        if (collider.gameObject.name == "Trophy") {
+            CMDebug.TextPopup("Win!", transform.position);
+        }
+    }
+
+    public void DamageKnockback(Vector3 knockbackDir, float knockbackDistance) {
+        transform.position += knockbackDir * knockbackDistance;
+    }
+
+    public Vector3 GetPosition() {
+        return transform.position;
+    }
+
+    public void Damage(EnemyHandler enemyHandler) {
+    }
+
+    public void Damage(CharacterWaypointsHandler enemyHandler) {
+        Damage(enemyHandler.GetPosition());
+    }
+
+    public void Damage(Vector3 attackerPosition) {
+        Vector3 bloodDir = (GetPosition() - attackerPosition).normalized;
+        Blood_Handler.SpawnBlood(GetPosition(), bloodDir);
+        // Knockback
+        transform.position += bloodDir * 1.5f;
+        health--;
+        if (health == 0) {
+            FlyingBody.Create(GameAssets.i.pfEnemyFlyingBody, GetPosition(), bloodDir);
+            gameObject.SetActive(false);
+            //transform.Find("Body").gameObject.SetActive(false);
+        }
+    }
+
+    public bool IsDead() {
+        return health <= 0;
+    }
+
+}
